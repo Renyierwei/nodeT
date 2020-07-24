cfa6e72d4f608bc5167f31fc0e8a0d43b7e1b168
zhh <2595682484@qq.com>
2020-07-24 14:58:58 +0800

    v1

---
+++ count.js
@@ -0,0 +1,22 @@
+
+var counter = function(arr) {
+    return "There are " + arr.length + " elements in the array";
+};
+
+
+var adder = function(a,b){
+    return `the sum of these 2 numbers ${a+b}`;
+};
+
+
+
+
+
+var pi = 3.14;
+
+
+module.exports = {
+    counter : counter,
+    adder : adder,
+    pi : pi
+}
